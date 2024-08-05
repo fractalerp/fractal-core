da8f613aa94a2aa9f2dc36ca20e3139a0306f0cf
Acellam Guy <acellam.guy@gmail.com>
2024-08-05 16:06:13 +0300

    chore(mode): implement base model class

---
+++ backend/app/models/nosql_model.ts
@@ -0,0 +1,6 @@
+import { NoSqlActiveRecord } from "../activerecord";
+
+export class NoSqlModel<T> extends NoSqlActiveRecord<T> {
+
+}
+
---
+++ backend/app/models/relation_model.ts
@@ -0,0 +1,6 @@
+import { RelationalActiveRecord } from "../activerecord";
+
+export class RelationalModel<T> extends RelationalActiveRecord<T> {
+
+}
+
